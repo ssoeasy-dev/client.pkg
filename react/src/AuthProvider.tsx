ba5cd903aafcf3a947598d8345d0d4fcc8547310
MoreWiktor <morewiktor@yandex.ru>
2026-02-24 16:56:45 +0300

    feat: react package

---
+++ react/src/AuthProvider.tsx
@@ -0,0 +1,20 @@
+import React, { useMemo } from "react"
+import { AuthConfig, AuthManager, AuthState } from "@ssoeasy-dev/core";
+import { useEffect, useState } from "react";
+import { AuthContext } from "./context";
+
+export const AuthProvider: React.FC<{
+  config: AuthConfig;
+  children: React.ReactNode;
+}> = ({ config, children }) => {
+  // const auth = new AuthManager(config);
+  const auth = useMemo(() => new AuthManager(config), [config.serviceId, config.redirectUri]);
+  const [state, setState] = useState<AuthState>(auth.getState());
+
+  useEffect(() => {
+    const unsubscribe = auth.onStateChange(setState);
+    return unsubscribe;
+  }, [auth]);
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
